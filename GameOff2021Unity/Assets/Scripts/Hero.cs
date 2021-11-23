1cf3c731bfb6a6096ada5d5d5c282642923effe9
Aljon Pineda <aljonjbpineda@gmail.com>
2021-11-22 21:48:48 -0500

    Fix UI

--- GameOff2021Unity/Assets/Scripts/Hero.cs
+++ GameOff2021Unity/Assets/Scripts/Hero.cs
@@ -6,28 +6,16 @@ public class Hero : Combatant
   [SerializeField] private int heroId;
   [SerializeField] private float spotlightDistance;
 
-  private Animator animator;
-  private static readonly int isAttacking = Animator.StringToHash("isAttacking");
-
   public int HeroId => heroId;
 
   protected override void Awake()
   {
     base.Awake();
 
-    animator = GetComponent<Animator>();
-
     if (heroId == 3)
     {
       Die();
     }
-  }
-
-  protected override void ChangeState(State state)
-  {
-    base.ChangeState(state);
-
-    animator.SetBool(isAttacking, currentState == State.Attacking);
   }
 
   public void Spotlight()
