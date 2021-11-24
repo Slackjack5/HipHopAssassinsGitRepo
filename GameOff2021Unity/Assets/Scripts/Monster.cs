fb3ac2c0fb41912158fbe2b4fdd94525aac8f6a3
Nycelly <dkr264@nyu.edu>
2021-11-24 02:04:09 -0500

    Implemented Cockroaches

--- GameOff2021Unity/Assets/Scripts/Monster.cs
+++ GameOff2021Unity/Assets/Scripts/Monster.cs
@@ -5,4 +5,10 @@ public class Monster : Combatant
   [SerializeField] private int patternId;
 
   public int PatternId => patternId;
+
+
+  public void endHitAnimation()
+  {
+    gameObject.GetComponent<Animator>().SetBool("Hurt", false);
+  }
 }
