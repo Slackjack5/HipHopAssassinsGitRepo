2a3f563f786120b8dddf107cc80d92b95a83544d
Aljon Pineda <aljonjbpineda@gmail.com>
2021-11-26 21:23:45 -0500

    Add camera shake on attack hit

--- GameOff2021Unity/Assets/Scripts/DamageNumber.cs
+++ GameOff2021Unity/Assets/Scripts/DamageNumber.cs
@@ -6,6 +6,10 @@ public class DamageNumber : MonoBehaviour
 {
   [SerializeField] private float displayDuration;
   [SerializeField] private float distance;
+  [SerializeField] private Color hugeColor;
+  [SerializeField] private Color largeColor;
+  [SerializeField] private Color mediumColor;
+  [SerializeField] private Color smallColor;
 
   [HideInInspector] public int value;
 
@@ -24,6 +28,27 @@ public class DamageNumber : MonoBehaviour
   {
     textComponent.text = value.ToString();
     textComponent.DOFade(0, displayDuration);
+
+    if (value >= 16)
+    {
+      textComponent.fontSize = 64;
+      textComponent.color = hugeColor;
+    }
+    else if (value >= 8)
+    {
+      textComponent.fontSize = 36;
+      textComponent.color = largeColor;
+    }
+    else if (value >= 4)
+    {
+      textComponent.fontSize = 24;
+      textComponent.color = mediumColor;
+    }
+    else
+    {
+      textComponent.fontSize = 14;
+      textComponent.color = smallColor;
+    }
 
     rectTransform.DOAnchorPosY(distance, displayDuration).OnComplete(() => Destroy(gameObject));
   }
