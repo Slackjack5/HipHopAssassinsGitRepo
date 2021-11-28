2bb6664f2b911329e66c34316e469f02993d555b
Nycelly <dkr264@nyu.edu>
2021-11-27 20:55:02 -0500

    Implemented new song and hero 1 button

---
+++ GameOff2021Unity/Assets/Scripts/BeatFX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class BeatFX : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  public void BumpOnBeat()
+  {
+    //CameraShaker.Instance.ShakeOnce(2, 2f, .1f, .1f);
+  }
+}
